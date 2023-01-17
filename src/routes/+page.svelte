3320adaefa8b324468977ceca73713710e74b705
robmal-0 <robmal-0@student.ltu.se>
2023-01-17 16:48:31 +0000

    Added tailwind

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,4 @@
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<p>Visit <a href="/test">/test</a> to see hej</p>
+<p class="bg-red-600 text-white">Testing</p>
