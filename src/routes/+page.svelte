b599472a2909aa7f56354541456beb01f8a70087
MattiasOz <mattiasjosefoz@gmail.com>
2023-01-27 12:48:24 +0000

    Added product-card + fan pic

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,41 @@
+<script>
+	import ProdCard from "./ProdCard.svelte";
+
+</script>
+
+<style>
+    .productSection{
+        display: flex;
+        justify-content: right;
+    }
+
+    .productSectionSub{
+        width: 80%;
+
+        display: grid;
+        grid-template-columns: 1fr 1fr 1fr 1fr 1fr; /* displays five products per row */
+    }
+
+    @media screen and (max-width: 700px){
+        .productSection {
+            grid-template-columns: 1fr 1fr 1fr;
+        }
+    }
+</style>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 <p>Visit <a href="/test">/test</a> to see hej</p>
 <p class="bg-red-600 text-white">Testing</p>
+
+<div class="productSection">
+    <div class="productSectionSub">
+        <ProdCard img="fan.webp" name="Noctua" price="200 sek"/>
+        <ProdCard img="lion.webp" name="Simba" price="2000000 sek"/>
+        <ProdCard img="fan.webp" name="Noctua" price="200 sek"/>
+        <ProdCard img="lion.webp" name="Simba" price="2000000 sek"/>
+        <ProdCard img="fan.webp" name="Noctua" price="200 sek"/>
+        <ProdCard img="lion.webp" name="Simba" price="2000000 sek"/>
+    </div>
+
+</div>
---
+++ src/routes/ProdCard.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+    let name:string;
+    let price:string;
+    let img:string;
+    let link:string;
+
+    export {name, price, img, link};
+</script>
+
+<a href={link}>
+    <div class="ProdCard bg-gray-400 m-2 border-2 border-red-600 grid grid-rows-5 aspect-[9/12]">
+        <div class="thumbnail row-span-4 m-4">
+            <img src={img} alt="Img Err" class="object-scale-down h-full w-full">
+        </div>
+        <div class="productInfo p-2">
+            <div class="prodName float-left">
+                {name}
+            </div>
+            <div class="prodPrice float-right">
+                {price}
+            </div>
+        </div>
+    </div>
+</a>
