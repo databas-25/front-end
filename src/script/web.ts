8a94bc9c861a29df207abbb0bacaf3ef0fad8426
robmal-0 <robmal-0@student.ltu.se>
2023-02-20 16:19:16 +0100

    fix url... again

--- src/script/web.ts
+++ src/script/web.ts
@@ -1,11 +1,19 @@
 import { ajax } from 'jquery';
 import { page } from '$app/stores';
 
-let ip = "localhost";
-page.subscribe(p => ip = p.url.hostname);
 type roots = 'user' | 'cart' | 'order' | 'product';
 
 export default function(path: string, data: unknown, success: JQuery.Ajax.SuccessCallback<unknown>, error: JQuery.Ajax.ErrorCallback<unknown>, root: roots = 'user') {
+    let ip;
+    switch (window.location.hostname) {
+        case 'onlyfans.beachore.com':
+            ip = 'onlyfans_api.beachore.com';
+            break;
+        default:
+            ip = window.location.hostname ?? 'localhost';
+            break;
+    }
+
     ajax({
         url: `http://${ip}:8000/${root}/${path}`,
         method: 'POST',
