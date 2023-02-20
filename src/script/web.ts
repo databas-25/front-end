8f8e0cb13a64fe982376c2e61dbc432fcf1d089e
robmal-0 <robmal-0@student.ltu.se>
2023-02-20 16:08:22 +0100

    Fix ip

--- src/script/web.ts
+++ src/script/web.ts
@@ -1,11 +1,13 @@
 import { ajax } from 'jquery';
+import { page } from '$app/stores';
 
-const IP = "localhost"
+let ip = "localhost";
+page.subscribe(p => ip = p.url.hostname);
 type roots = 'user' | 'cart' | 'order' | 'product';
 
 export default function(path: string, data: unknown, success: JQuery.Ajax.SuccessCallback<unknown>, error: JQuery.Ajax.ErrorCallback<unknown>, root: roots = 'user') {
     ajax({
-        url: `http://${IP}:8000/${root}/${path}`,
+        url: `http://${ip}:8000/${root}/${path}`,
         method: 'POST',
         dataType: 'json',
         contentType: 'application/json; charset=UTF-8',
